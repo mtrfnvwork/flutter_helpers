46351332cf722fb6f7e6db66623353303cea9424
mtrfnv <mtrfnvwork@gmail.com>
2022-06-26 21:17:33 +0500

    Добавлены хелперы для навигатора

--- lib/flutter/helpers/navigator_helper.dart
+++ lib/flutter/helpers/navigator_helper.dart
@@ -1,7 +1,60 @@
-// todo
-// Navigator.push
-// Navigator.pushReplacement
-// Navigator.pushAndRemoveUntil
-// Navigator.canPop
-// Navigator.maybePop
-// Navigator.popUntil
+import 'package:flutter/material.dart';
+
+Future<T?> push<T extends Object?>(
+  BuildContext context,
+  Widget page, {
+  bool fullscreenDialog = false,
+}) {
+  return Navigator.push(
+    context,
+    MaterialPageRoute(
+      builder: (context) => page,
+      fullscreenDialog: fullscreenDialog,
+    ),
+  );
+}
+
+Future<T?> pushReplacement<T extends Object?, TO extends Object?>(
+  BuildContext context,
+  Widget page, {
+  TO? result,
+  bool fullscreenDialog = false,
+}) {
+  return Navigator.pushReplacement<T, TO>(
+    context,
+    MaterialPageRoute(
+      builder: (context) => page,
+      fullscreenDialog: fullscreenDialog,
+    ),
+    result: result,
+  );
+}
+
+Future<T?> pushAndRemoveUntil<T extends Object?>(
+  BuildContext context,
+  Widget page,
+  RoutePredicate predicate, {
+  bool fullscreenDialog = false,
+}) {
+  return Navigator.pushAndRemoveUntil<T>(
+    context,
+    MaterialPageRoute(
+      builder: (context) => page,
+      fullscreenDialog: fullscreenDialog,
+    ),
+    predicate,
+  );
+}
+
+bool canPop(BuildContext context) {
+  var navigator = Navigator.maybeOf(context);
+  return navigator != null && navigator.canPop();
+}
+
+Future<bool> maybePop<T extends Object?>(BuildContext context, [T? result]) {
+  return Navigator.maybePop<T>(context, result);
+}
+
+void popUntil(BuildContext context, RoutePredicate predicate) {
+  Navigator.popUntil(context, predicate);
+}
