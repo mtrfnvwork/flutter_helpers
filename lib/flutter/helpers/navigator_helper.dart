1714094f55b879c3c142c919f95aabc01e183946
mtrfnv <mtrfnvwork@gmail.com>
2022-06-27 05:20:53 +0500

    Добавлен метод pop

--- lib/flutter/helpers/navigator_helper.dart
+++ lib/flutter/helpers/navigator_helper.dart
@@ -46,6 +46,14 @@ Future<T?> pushAndRemoveUntil<T extends Object?>(
   );
 }
 
+void pop<T extends Object?>(BuildContext context, [T? result]) {
+  Navigator.pop<T>(context, result);
+}
+
+void popUntil(BuildContext context, RoutePredicate predicate) {
+  Navigator.popUntil(context, predicate);
+}
+
 bool canPop(BuildContext context) {
   var navigator = Navigator.maybeOf(context);
   return navigator != null && navigator.canPop();
@@ -53,8 +61,4 @@ bool canPop(BuildContext context) {
 
 Future<bool> maybePop<T extends Object?>(BuildContext context, [T? result]) {
   return Navigator.maybePop<T>(context, result);
-}
-
-void popUntil(BuildContext context, RoutePredicate predicate) {
-  Navigator.popUntil(context, predicate);
 }
