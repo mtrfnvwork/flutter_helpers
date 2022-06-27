523a3ee2aa69cf0dfe6bbdc39ad636fe0e528084
mtrfnv <mtrfnvwork@gmail.com>
2022-06-27 07:46:40 +0500

    Добавлены экспорты

--- lib/flutter_helpers.dart
+++ lib/flutter_helpers.dart
@@ -2,6 +2,13 @@ library flutter_helpers;
 
 export 'package:flutter_helpers/dart/extensions/iterable_extension.dart';
 export 'package:flutter_helpers/dart/extensions/string_extension.dart';
+export 'package:flutter_helpers/flutter/helpers/focus_helper.dart';
 export 'package:flutter_helpers/flutter/helpers/navigator_helper.dart';
+export 'package:flutter_helpers/flutter/widgets/center_text.dart';
+export 'package:flutter_helpers/flutter/widgets/check_box_button.dart';
+export 'package:flutter_helpers/flutter/widgets/dialog_button.dart';
 export 'package:flutter_helpers/flutter/widgets/h.dart';
+export 'package:flutter_helpers/flutter/widgets/page_container.dart';
+export 'package:flutter_helpers/flutter/widgets/radio_button.dart';
+export 'package:flutter_helpers/flutter/widgets/tap_detector.dart';
 export 'package:flutter_helpers/flutter/widgets/w.dart';
