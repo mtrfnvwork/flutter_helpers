5355582fdfd9f132bcd1aa29fcd31f2b43457aea
mtrfnv <mtrfnvwork@gmail.com>
2022-07-02 22:49:57 +0500

    Изменение текста кнопок в диалоговом окне для редактирования текста + Рефакторинг

--- lib/flutter_helpers.dart
+++ lib/flutter_helpers.dart
@@ -6,50 +6,48 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 part 'package:flutter_helpers/dart/extensions/date_time_extension.dart';
-
 part 'package:flutter_helpers/dart/extensions/iterable_extension.dart';
-
 part 'package:flutter_helpers/dart/extensions/string_extension.dart';
-
 part 'package:flutter_helpers/flutter/helpers/focus_helper.dart';
-
 part 'package:flutter_helpers/flutter/helpers/navigator_helper.dart';
-
 part 'package:flutter_helpers/flutter/helpers/state_helper.dart';
-
 part 'package:flutter_helpers/flutter/widgets/center_text.dart';
-
 part 'package:flutter_helpers/flutter/widgets/check_box_button.dart';
-
 part 'package:flutter_helpers/flutter/widgets/dialog_button.dart';
-
 part 'package:flutter_helpers/flutter/widgets/dialogs/text_form_field_dialog.dart';
-
 part 'package:flutter_helpers/flutter/widgets/dialogs/yes_no_dialog.dart';
-
 part 'package:flutter_helpers/flutter/widgets/h.dart';
-
 part 'package:flutter_helpers/flutter/widgets/page_container.dart';
-
 part 'package:flutter_helpers/flutter/widgets/radio_button.dart';
-
 part 'package:flutter_helpers/flutter/widgets/tap_detector.dart';
-
 part 'package:flutter_helpers/flutter/widgets/w.dart';
 
-part 'package:flutter_helpers/flutter/widgets/widget_settings.dart';
+part 'package:flutter_helpers/widget_settings.dart';
 
 class FlutterHelpers {
   static void changeYesNoDialogSettings({
-    String? defaultNoButtonText,
-    String? defaultYesButtonText,
+    String? noButtonText,
+    String? yesButtonText,
   }) {
-    if (defaultNoButtonText != null) {
-      YesNoDialogDialogSettings.noButtonText = defaultNoButtonText;
+    if (noButtonText != null) {
+      YesNoDialogDialogSettings.noButtonText = noButtonText;
     }
 
-    if (defaultYesButtonText != null) {
-      YesNoDialogDialogSettings.yesButtonText = defaultYesButtonText;
+    if (yesButtonText != null) {
+      YesNoDialogDialogSettings.yesButtonText = yesButtonText;
+    }
+  }
+
+  static void textFormFieldDialogSettings({
+    String? cancelButtonText,
+    String? confirmButtonText,
+  }) {
+    if (cancelButtonText != null) {
+      TextFormFieldDialogSettings.cancelButtonText = cancelButtonText;
+    }
+
+    if (confirmButtonText != null) {
+      TextFormFieldDialogSettings.confirmButtonText = confirmButtonText;
     }
   }
 }
