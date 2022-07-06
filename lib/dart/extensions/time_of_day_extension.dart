cbf9be8f975ff337175bd33117f38f3e4e5bcd41
mtrfnv <mtrfnvwork@gmail.com>
2022-07-06 17:22:05 +0500

    Добавлены операторы для сравнения TimeOfDay

--- lib/dart/extensions/time_of_day_extension.dart
+++ lib/dart/extensions/time_of_day_extension.dart
@@ -2,4 +2,12 @@ import 'package:flutter/material.dart';
 
 extension TimeOfDayExtension on TimeOfDay {
   Duration toDuration() => Duration(hours: hour, minutes: minute);
+
+  bool operator >(TimeOfDay other) => hour * 60 + minute > other.hour * 60 + other.minute;
+
+  bool operator <(TimeOfDay other) => hour * 60 + minute < other.hour * 60 + other.minute;
+
+  bool operator >=(TimeOfDay other) => hour * 60 + minute >= other.hour * 60 + other.minute;
+
+  bool operator <=(TimeOfDay other) => hour * 60 + minute <= other.hour * 60 + other.minute;
 }
