8cd48038920833b151aed9457878cc746096e6d1
mtrfnv <mtrfnvwork@gmail.com>
2022-07-08 05:54:06 +0500

    Добавлены операторы для DateTime

--- lib/dart/extensions/date_time_extension.dart
+++ lib/dart/extensions/date_time_extension.dart
@@ -2,4 +2,14 @@ part of 'package:flutter_helpers/flutter_helpers.dart';
 
 extension DateTimeExtension on DateTime {
   String format([String? newPattern, String? locale]) => DateFormat(newPattern, locale).format(this);
+
+  Duration operator -(DateTime other) => difference(other);
+
+  bool operator >(DateTime other) => isAfter(other);
+
+  bool operator <(DateTime other) => isBefore(other);
+
+  bool operator >=(DateTime other) => isAfter(other) || isAtSameMomentAs(other);
+
+  bool operator <=(DateTime other) => isBefore(other) || isAtSameMomentAs(other);
 }
