c9a2764a90aa6656909d259937a6eed6c943efa4
mtrfnv <mtrfnvwork@gmail.com>
2022-07-10 01:42:42 +0500

    Правки

--- lib/dart/extensions/string_extension.dart
+++ lib/dart/extensions/string_extension.dart
@@ -1,5 +1,5 @@
 part of 'package:flutter_helpers/flutter_helpers.dart';
 
 extension StringExtension on String? {
-  bool get isNullOrEmpty => this?.isNotEmpty != false;
+  bool get isNullOrEmpty => this?.isNotEmpty != true;
 }
