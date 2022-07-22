4a7e8bfd89dc5933a1ae3018734e623e6dfe9d94
mtrfnv <mtrfnvwork@gmail.com>
2022-07-23 04:35:39 +0500

    Изменено стандартное время исчезания и появления HiddenWidget

--- lib/flutter/widgets/hidden_widget.dart
+++ lib/flutter/widgets/hidden_widget.dart
@@ -5,7 +5,7 @@ class HiddenWidget extends StatelessWidget {
     Key? key,
     required this.hide,
     required this.child,
-    this.animationDuration = const Duration(milliseconds: 300),
+    this.animationDuration = const Duration(milliseconds: 150),
   }) : super(key: key);
 
   final bool hide;
