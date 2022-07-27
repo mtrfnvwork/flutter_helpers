03980e6cae3cbf2a4cb8fd8772e7443afdfd076a
mtrfnv <mtrfnvwork@gmail.com>
2022-07-28 03:25:02 +0500

    Добавлено расширение для строк

--- lib/dart/extensions/string_extension.dart
+++ lib/dart/extensions/string_extension.dart
@@ -1,5 +1,12 @@
 part of 'package:flutter_helpers/flutter_helpers.dart';
 
-extension StringExtension on String? {
+extension NullableStringExtension on String? {
   bool get isNullOrEmpty => this?.isNotEmpty != true;
+}
+
+extension StringExtension on String {
+  String capitalizeFirst() {
+    assert(isNotEmpty);
+    return '${this[0].toUpperCase()}${substring(1)}';
+  }
 }
