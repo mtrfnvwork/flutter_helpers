a247bd70cea0cf77774a7b4245be207f9ccaec1d
mtrfnv <mtrfnvwork@gmail.com>
2022-07-30 05:16:34 +0500

    Правки

--- lib/flutter/widgets/dialogs/text_dialog.dart
+++ lib/flutter/widgets/dialogs/text_dialog.dart
@@ -4,12 +4,14 @@ Future<void> showTextDialog({
   required BuildContext context,
   String? title,
   required String text,
+  String okText = 'ОК',
 }) async {
   await showDialog(
     context: context,
     builder: (context) => _TextDialog(
       title: title,
       text: text,
+      okText: okText,
     ),
   );
 }
@@ -19,16 +21,24 @@ class _TextDialog extends StatelessWidget {
     Key? key,
     required this.title,
     required this.text,
+    required this.okText,
   }) : super(key: key);
 
   final String? title;
   final String text;
+  final String okText;
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
       title: title != null ? Text(title!) : null,
       content: Text(text),
+      actions: [
+        DialogButton(
+          text: okText,
+          onPressed: () => pop(context),
+        ),
+      ],
     );
   }
 }
