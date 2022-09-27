28d840e4e081e28feece6900b821a16a78078211
mtrfnvwork <mtrfnvwork@gmail.com>
2022-09-27 06:42:26 +0500

    Добавлены методы getScreenWidth и getScreenHeight

--- lib/flutter/helpers/media_query_helper.dart
+++ lib/flutter/helpers/media_query_helper.dart
@@ -1,5 +1,9 @@
 part of 'package:flutter_helpers/flutter_helpers.dart';
 
+double getScreenWidth(BuildContext context) => MediaQuery.of(context).size.width;
+
+double getScreenHeight(BuildContext context) => MediaQuery.of(context).size.height;
+
 double getTopPadding(BuildContext context) => MediaQuery.of(context).padding.top;
 
 double getBottomPadding(BuildContext context) => MediaQuery.of(context).padding.bottom;
