d614b3ce8d6e45961550aff3c08025ac10934bc3
mtrfnvwork <mtrfnvwork@gmail.com>
2022-10-21 02:42:35 +0500

    Правки

--- lib/flutter_helpers.dart
+++ lib/flutter_helpers.dart
@@ -11,10 +11,11 @@ part 'package:flutter_helpers/dart/extensions/double_extension.dart';
 part 'package:flutter_helpers/dart/extensions/int_to_duration_extension.dart';
 part 'package:flutter_helpers/dart/extensions/iterable_extensions/date_time_iterable_extension.dart';
 part 'package:flutter_helpers/dart/extensions/iterable_extensions/iterable_extension.dart';
-
 part 'package:flutter_helpers/dart/extensions/iterable_extensions/nullable_iterable_extension.dart';
-
 part 'package:flutter_helpers/dart/extensions/iterable_extensions/numeric_iterable_extension.dart';
+
+part 'package:flutter_helpers/dart/extensions/map_extensions/nullable_map_extension.dart';
+
 part 'package:flutter_helpers/dart/extensions/num_extension.dart';
 part 'package:flutter_helpers/dart/extensions/string_extensions/nullable_string_extension.dart';
 part 'package:flutter_helpers/dart/extensions/string_extensions/string_extension.dart';
