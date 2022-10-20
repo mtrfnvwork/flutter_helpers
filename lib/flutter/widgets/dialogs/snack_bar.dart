463be2023f38c0526fff9dffac74c8d03d93367d
mtrfnvwork <mtrfnvwork@gmail.com>
2022-10-21 02:56:27 +0500

    Возможность задавать конфигурацию при вызове SnackBar

--- lib/flutter/widgets/dialogs/snack_bar.dart
+++ lib/flutter/widgets/dialogs/snack_bar.dart
@@ -1,14 +1,21 @@
 part of 'package:flutter_helpers/flutter_helpers.dart';
 
-void showSnackBar(BuildContext context, String text) {
+void showSnackBar(
+  BuildContext context,
+  String text, {
+  SnackBarConfiguration? configuration,
+}) {
+  var backgroundColor = configuration?.backgroundColor ?? WidgetConfiguration.snackBarConfiguration.backgroundColor;
+  var textStyle = configuration?.textStyle ?? WidgetConfiguration.snackBarConfiguration.textStyle;
+
   ScaffoldMessenger.of(context)
     ..hideCurrentSnackBar()
     ..showSnackBar(
       SnackBar(
-        backgroundColor: WidgetConfiguration.snackBarConfiguration.backgroundColor,
+        backgroundColor: backgroundColor,
         content: Text(
           text,
-          style: WidgetConfiguration.snackBarConfiguration.textStyle,
+          style: textStyle,
         ),
       ),
     );
