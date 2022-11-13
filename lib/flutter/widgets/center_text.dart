160e6d274883a2f72ba25623a858ab7f962ed2d3
mtrfnvwork <mtrfnvwork@gmail.com>
2022-11-14 01:37:53 +0500

    В CenterText добавлен constraints

--- lib/flutter/widgets/center_text.dart
+++ lib/flutter/widgets/center_text.dart
@@ -7,16 +7,18 @@ class CenterText extends StatelessWidget {
     this.padding,
     this.margin,
     this.style,
+    this.constraints,
   }) : super(key: key);
 
   final String text;
   final EdgeInsets? padding;
   final EdgeInsets? margin;
   final TextStyle? style;
+  final BoxConstraints? constraints;
 
   @override
   Widget build(BuildContext context) {
-    return Center(
+    var child = Center(
       child: Container(
         padding: padding ?? WidgetConfiguration.centerTextConfiguration.padding,
         margin: margin ?? WidgetConfiguration.centerTextConfiguration.margin,
@@ -27,5 +29,14 @@ class CenterText extends StatelessWidget {
         ),
       ),
     );
+
+    if (constraints != null) {
+      return ConstrainedBox(
+        constraints: constraints!,
+        child: child,
+      );
+    }
+
+    return child;
   }
 }
