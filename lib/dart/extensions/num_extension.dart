432d45d4f45e571debe3fb5d13d6c3d68c0c29bb
mtrfnvwork <mtrfnvwork@gmail.com>
2023-05-03 01:34:04 +0500

    Правки

--- lib/dart/extensions/num_extension.dart
+++ lib/dart/extensions/num_extension.dart
@@ -2,7 +2,7 @@ part of 'package:flutter_helpers/flutter_helpers.dart';
 
 extension NumExtension on num {
   bool isBetween(num from, num to) {
-    assert(to > from);
+    assert(to >= from);
     return this >= from && this <= to;
   }
 }
