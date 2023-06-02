ae40279146fd8009d43e972290d3a9b6bf4ffe4f
mtrfnvwork <mtrfnvwork@gmail.com>
2023-06-03 01:08:41 +0500

    .

--- lib/flutter/helpers/parsers.dart
+++ lib/flutter/helpers/parsers.dart
@@ -1,10 +1,16 @@
 part of 'package:flutter_helpers/flutter_helpers.dart';
 
-TimeOfDay parseTimeOfDay(String input) {
-  var split = input.split(':');
+TimeOfDay? tryParseTimeOfDay(String input) {
+  try {
+    var split = input.split(':');
 
-  return TimeOfDay(
-    hour: int.parse(split[0]),
-    minute: int.parse(split[1]),
-  );
+    return TimeOfDay(
+      hour: int.parse(split[0]),
+      minute: int.parse(split[1]),
+    );
+  } catch (_) {
+    return null;
+  }
 }
+
+TimeOfDay parseTimeOfDay(String input) => tryParseTimeOfDay(input)!;
