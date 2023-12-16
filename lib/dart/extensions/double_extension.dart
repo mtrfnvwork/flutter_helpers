311e5912e2a1650806a2daab6e1b87ab55d8f147
mtrfnvwork <61468224+mtrfnvwork@users.noreply.github.com>
2023-12-16 05:10:48 +0500

    Правки

--- lib/dart/extensions/double_extension.dart
+++ lib/dart/extensions/double_extension.dart
@@ -1,10 +1,12 @@
 part of 'package:flutter_helpers/flutter_helpers.dart';
 
 extension DoubleExtension on double {
+  @Deprecated(
+      'Не дает адекватных результатов из-за преобразования в экспоненциальную форму при большом количестве знаков после запятой')
   String trim({
     int? precision,
   }) {
-    var str = toStringAsFixed(precision ?? 20);
+    var str = precision == null ? toString() : toStringAsFixed(precision);
 
     if (str.contains('.')) {
       while (str.endsWith('0')) {
